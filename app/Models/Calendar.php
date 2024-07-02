edf1b70a6880e6a701226f29c4280593a3588516
Mohamad Hesari <mdhesari99@gmail.com>
2024-07-02 14:03:59 +0330

    feat: calendar, schedule entities

---
+++ app/Models/Calendar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Calendar extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'owner_id',
+        'workspace_id',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function workspace()
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+}
---
+++ app/Models/Schedule.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'availability_type',
+        'calendar_id',
+        'owner_id',
+        'starts_at',
+        'ends_at',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function calendar()
+    {
+        return $this->belongsTo(Calendar::class);
+    }
+}
