febde51abf68e771bc6b9e135a5b0c5bd246aab9
Mohamad Hesari <mdhesari99@gmail.com>
2024-07-09 15:44:46 +0330

    feat: notification model & migration

--- database/migrations/2024_07_03_150654_create_notifications_table.php
+++ database/migrations/2024_07_03_150654_create_notifications_table.php
@@ -1,26 +1,40 @@
 <?php
 
+use App\Enums\NotificationStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->nullableMorphs('notifiable');
+            $table->text('message')->nullable();
+
+            $table->enum('status', array_column(NotificationStatus::cases(), 'name'));
+
+            $table->softDeletes();
+
+            $table->timestamp('read_at')->nullable();
+            $table->timestamp('sends_at')->nullable();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('notifications');
     }
