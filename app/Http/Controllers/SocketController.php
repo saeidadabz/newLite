32c5a8a59776d88d3b9998eb6ab887f026448311
Katerou22 <xkaterou22@gmail.com>
2024-07-20 11:06:22 +0330

    Fix data json in activites.

--- app/Http/Controllers/SocketController.php
+++ app/Http/Controllers/SocketController.php
@@ -35,7 +35,7 @@ class SocketController extends Controller
                                                  'event_type'   => $event->event,
                                                  'workspace_id' => $event->room()->workspace->id,
                                                  'room_id'      => $event->room()->id,
-                                                 'data'         => $request->all(),
+                                                 'data'         => json_decode($request->all()),
                                              ]);
 
     }
