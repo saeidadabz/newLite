c2db2a8e696739ca008f6e15bdb404db78b5d687
katerou22 <xkaterou22@gmail.com>
2024-08-11 17:40:10 +0330

    Change acts sort.

--- app/Models/User.php
+++ app/Models/User.php
@@ -214,7 +214,7 @@ class User extends Authenticatable
 
     public function getTime($period = NULL)
     {
-        $acts = $this->activities();
+        $acts = $this->activities()->orderBy('id', 'DESC');
 
         if ($period === 'today') {
 
