7dd30abb52fa7938642258d229f73b10229f93e9
katerou22 <xkaterou22@gmail.com>
2024-08-20 01:47:16 +0330

    Fix updating last used at.

--- app/Utilities/PersonalAccessToken.php
+++ app/Utilities/PersonalAccessToken.php
@@ -3,8 +3,9 @@
 namespace App\Utilities;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-class PersonalAccessToken {
+class PersonalAccessToken extends SanctumPersonalAccessToken {
     protected function lastUsedAt(): Attribute {
         return Attribute::make(set: function (mixed $value): void {
             // disable updating the last_used_at attribute as it's not used
